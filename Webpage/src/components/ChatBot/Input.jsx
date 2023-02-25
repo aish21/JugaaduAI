bd2c6786f099da626e5da3f04249ffe61a8a6abd
Abhinaya Srinivasan <abhinaya.kms@gmail.com>
2023-02-25 22:53:29 +0800

    add file upload

--- Webpage/src/components/ChatBot/Input.jsx
+++ Webpage/src/components/ChatBot/Input.jsx
@@ -1,27 +1,63 @@
 import React, { useState } from "react";
+import { firebasestorage } from "../../firebase/initFirebase";
+import { uploadBytesResumable, ref as sRef, getDownloadURL } from "firebase/storage";
 
 export default function Input({ onSend }) {
   const [text, setText] = useState("");
+  const [file, setFile] = useState();
+  const [percent, setPercent] = useState(0);
 
   const handleInputChange = e => {
     setText(e.target.value);
   };
 
+  const handleFileChange = e => {
+    setFile(e.target.files[0]);
+    e.preventDefault();
+  }
+
   const handleSend = e => {
+    handleUpload();
     e.preventDefault();
     onSend(text);
     setText("");
   };
 
+  function handleUpload() {
+    if (!file) {
+      alert("Please choose a file first!");
+    }
+    const storageRef = sRef(firebasestorage, '/input/' + file.name);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const percent = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
+
+        setPercent(percent);
+      },
+      (err) => console.log(err),
+      () => {
+      // download url
+      getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+      console.log(url);
+    });
+  }
+  ); 
+  }
+
   return (
     <div className="input">
-      <form onSubmit={handleSend}>
+      <form onSubmit={handleSend} className="side-by-side">
         <input
           type="text"
           onChange={handleInputChange}
           value={text}
           placeholder="Enter your message here"
         />
+        <input type="file" onChange={handleFileChange}/>
         <button>
           <svg
             version="1.1"
